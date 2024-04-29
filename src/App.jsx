ae1e94cf83f0ded1b9025fc46510abc305c8d92f
Rohith vemula <vemularohith31@gmail.com>
2024-04-29 16:01:41 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NetflixMain from './pages/netflixmain'
-import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import './style.css'
 
